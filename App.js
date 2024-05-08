00ddd1430a813d1b5bf951f7448b2c64e76baf7d
Rafael Cortes Soares <NIT0303999@aluno.firjansenaisesi.com.br>
2024-05-08 13:28:10 -0300

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import firebase from "./src/firebaseConnection"
+import {useState, useEffect } from 'react';
+
 
 export default function App() {
+  const[email , setEmail] = useState("")
+  const[senha , setSenha] = useState ("")
+  const [usuario, setUsuario] = useState("")
+  const[type, setType] = useState("Login")
+  
+async function logar (){
+
+await firebase.auth().signInWithEmailAndPassword(email,senha).then((value) =>{
+  alert("Bem vindo " + value.user.email)
+  setUsuario(value.user.email)
+})
+.catch((error) =>{
+  alert("Deu ruim")
+  return;
+})
+
+}
+
+  async function cadastrar(){
+    await firebase.auth().createUserWithEmailAndPassword(email,senha).then((value) => {
+  alert("Usuario Criado " + value.user.email)      
+    })
+   .catch((error) => {
+     if(error.code=== 'auth/weak-password'){
+       alert("Sua senha devara possuir pelo menos 6 caracteres")   
+       return;
+     }
+     if(error.code=== 'auth/weak-email'){
+      alert("email invalido")   
+      return;
+     }
+     else {
+      alert("Deu tudo errado!")   
+      return;
+     }    
+   })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <TextInput style={styles.input}
+      onChangeText = {(texto) => setEmail(texto)}
+      value = {email}
+      placeholder = "Digite seu Email"
+      />
+      <TextInput style={styles.input}
+      onChangeText = {(texto) => setSenha(texto)}
+      value = {senha}
+      placeholder = "Digite sua senha"
+      />
+      {type === 'login' ?(
+        <Button
+        title='Acessar'
+        onPress={logar}
+        />
+      ): (
+        <Button 
+        title='cadastrar'
+        onPress={cadastrar}
+        />
+      )}
+  
+      {type === 'login' ?(
+        <Button
+        title='Não tenho conta'
+        onPress={() => setType ('cadastro')}
+        />
+      ): (
+        <Button 
+        title='já possuo conta'
+        onPress={() => setType('login')}
+        />
+      )}
+   </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +88,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap:20
   },
+    input : {
+      width:200,
+      borderWidth:2,
+      borderColor : 'black',
+      borderRadius:50,
+      padding: 5
+    }
 });
